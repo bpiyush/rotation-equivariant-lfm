bab8c397ef77045d1459991130882bbefd42d059
Piyush Bagad <piyushnbagad11@gmail.com>
2022-05-03 23:59:55 +0200

    Adds README to setup R2D2 training datasets

---
+++ DATASETS.md
@@ -0,0 +1,22 @@
+
+## Aachen Dataset (and variants for R2D2 training)
+
+1. Download the datasets (original tutorial [here](https://github.com/naver/r2d2#training-the-model))
+```sh
+bash download_aachen.sh -d /path/to/root/data/folder/ # e.g. ~/datasets/
+```
+
+2. Download R2D2 repository
+```sh
+mkdir lib/
+cd lib/
+git clone git@github.com:naver/r2d2.git
+cd -
+```
+
+3. Check sample data demo:
+```
+export PYTHONPATH=./lib/r2d2/
+python -m lib.r2d2.tools.dataloader "PairLoader(aachen_flow_pairs)"
+```
+This may not work on Lisa or a remote server with no GUI.
--- download_aachen.sh
+++ download_aachen.sh
@@ -3,14 +3,21 @@
 # get inputs from the user
 while getopts "d:" OPTION; do
     case $OPTION in
-        d) data_root=$OPTARG;;
+        d) DATA_ROOT=$OPTARG;;
         *) exit 1 ;;
     esac
 done
 
 # set data root default
-if [ "$data_root" ==  "" ];then
-       data_root=$HOME/datasets/
+if [ "$DATA_ROOT" ==  "" ];then
+        DATA_ROOT=$HOME/datasets/
 fi
 
-mkdir -p $data_root/
+mkdir -p $DATA_ROOT/
+ln -fs $DATA_ROOT data
+mkdir -p $DATA_ROOT/aachen/
+
+DRIVE_ID=18vz-nCBFhyxiX7s-zynGczvtPeilLBRB
+URL=https://drive.google.com/uc?id=$DRIVE_ID
+gdown $URL -O $DATA_ROOT/aachen/database_and_query_images.zip
+bash download_r2d2_training_data.sh 
