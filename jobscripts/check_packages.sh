de838119d4d7170baeb64424746c514596add028
Piyush Bagad <piyushnbagad11@gmail.com>
2022-06-04 07:57:53 +0200

    WIP: testing jobscript

---
+++ jobscripts/check_packages.sh
@@ -0,0 +1,19 @@
+### Checks if all packages are installed in the conda environment
+
+#SBATCH --partition=gpu_shared_course
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=R2D2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time00:10:00
+#SBATCH --mem=32000M
+#SBATCH --output=slurm_output_%A.out
+
+module purge
+module load 2021
+module load Anaconda3/2021.05
+
+source deactivate
+source activate relfm-v1.0
+
+python setup/check_packages.py
--- jobscripts/create_gpu_env.sh
+++ jobscripts/create_gpu_env.sh
@@ -19,12 +19,18 @@ echo "----------------------------------------"
 # get path to the repository
 repository_path=$(git rev-parse --show-toplevel)
 echo "repository_path: $repository_path"
-# cd $HOME/rotation-equivariant-lfm/
 cd $repository_path
-conda env create -f relfm-v1.0.yml
-# conda create -y -n relfm-v1.0 python=3.9
-# source activate relfm-v1.0
-# conda install -y tqdm pillow numpy matplotlib scipy
-# pip install ipdb ipython jupyter jupyterlab gdown termcolor natsort opencv-python
-# pip install torch==1.8.1+cu101 torchvision==0.9.1+cu101 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+# NOTE: This does not work well since some dependencies are pip
+# and others are conda
+# conda env create -f relfm-v1.0.yml
+
+conda create -y -n relfm-v1.0 python=3.9
+source activate relfm-v1.0
+conda install -y tqdm pillow numpy matplotlib scipy
+pip install ipdb ipython jupyter jupyterlab gdown termcolor natsort opencv-python
+pip install torch==1.8.1+cu101 \
+    torchvision==0.9.1+cu101 \
+    torchaudio==0.8.1 \
+    -f https://download.pytorch.org/whl/torch_stable.html
 echo "----------------------------------------"
